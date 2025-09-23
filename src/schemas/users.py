abacd10197faa442559d72d40f0be243a83a451f
garik <igorkireev02@mail.ru>
2025-09-23 10:30:29 +0300

    upd schemas

--- src/schemas/users.py
+++ src/schemas/users.py
@@ -6,10 +6,28 @@ class RelationshipStatus(StrEnum):
     IN_RELATIONSHIP = "in relationship"
     MARRIED = "married"
 
-class User(BaseModel):
+class UserBase(BaseModel):
     name: str = Field(min_length=2, max_length=15)
     surname: str = Field(min_length=2, max_length=15)
     age: int = Field(lt=100)
-    hobbies: str | None
+    hobbies: str
     relationship_status: RelationshipStatus
 
+
+class UserCreate(UserBase):
+    pass
+
+
+class User(UserBase):
+    id: int
+
+    class Config:
+        from_attributes = True
+
+
+class UserUpdate(BaseModel):
+    name: str | None = Field(None, min_length=2, max_length=15)
+    surname: str | None = Field(None, min_length=2, max_length=15)
+    age: int | None = Field(None, lt=100)
+    hobbies: str | None = None
+    relationship_status: RelationshipStatus | None = None
