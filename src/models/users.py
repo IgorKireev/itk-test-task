9fffe07f766d1afbc1a4da525b44c26958f3d607
garik <igorkireev02@mail.ru>
2025-09-23 10:18:34 +0300

    upd models

--- src/models/users.py
+++ src/models/users.py
@@ -14,7 +14,7 @@ class UsersOrm(Base):
     name: Mapped[str] = mapped_column(String(15))
     surname: Mapped[str] = mapped_column(String(15))
     age: Mapped[int] = mapped_column(Integer, CheckConstraint("age < 100"))
-    hobbies: Mapped[str | None] = mapped_column(String(100))
+    hobbies: Mapped[str] = mapped_column(String(100))
     relationship_status: Mapped[RelationshipStatus] = mapped_column(
         Enum(RelationshipStatus, values_callable=lambda x: [e.value for e in x])
     )
