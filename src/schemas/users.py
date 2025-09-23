fd3b403695809248aa718a3923f5b7a9367dc573
garik <igorkireev02@mail.ru>
2025-09-23 09:22:22 +0300

    add pydantic schemas

---
+++ src/schemas/users.py
@@ -0,0 +1,15 @@
+from enum import StrEnum
+from pydantic import BaseModel, Field
+
+class RelationshipStatus(StrEnum):
+    SINGLE = "single"
+    IN_RELATIONSHIP = "in relationship"
+    MARRIED = "married"
+
+class User(BaseModel):
+    name: str = Field(min_length=2, max_length=15)
+    surname: str = Field(min_length=2, max_length=15)
+    age: int = Field(lt=100)
+    hobbies: str | None
+    relationship_status: RelationshipStatus | None
+
