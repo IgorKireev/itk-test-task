c2275e184c16f2451288ed3a6c0fdc91e5dcce18
garik <igorkireev02@mail.ru>
2025-09-24 09:10:48 +0300

    upd env.py

--- src/migrations/env.py
+++ src/migrations/env.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+
+
 from logging.config import fileConfig
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
@@ -11,7 +17,7 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-config.set_main_option("sqlalchemy.url", settings.DATABASE_URL_asyncpg + "?async_fallback=True")
+config.set_main_option("sqlalchemy.url", settings.database_url_asyncpg + "?async_fallback=True")
 
 target_metadata = Base.metadata
 
